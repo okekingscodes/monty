880c280251d3ee2581d26d6b0d511bca17faa549
Odim Kingsley <okekings@gmail.com>
2022-10-05 17:45:47 +0100

    Print all values on 'stack' starting from the top

--- pall.c
+++ pall.c
@@ -1,21 +1,26 @@
 #include "monty.h"
-/**
- * f_pall - prints the stack
- * @head: stack head
- * @counter: no used
- * Return: no return
-*/
-void f_pall(stack_t **head, unsigned int counter)
-{
-	stack_t *h;
-	(void)counter;
 
-	h = *head;
-	if (h == NULL)
-		return;
-	while (h)
+/**
+ * m_pall - print all values on `stack' starting from the top
+ * @stack: double pointer to head of stack
+ * @line_number: line number being executed from script file
+ *
+ * Return: void
+ */
+void m_pall(stack_t **stack, unsigned int line_number)
+{
+	stack_t *head;
+
+	(void)(line_number);
+
+	head = *stack;
+	while (head != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
+		printf("%d\n", head->n);
+		head = head->next;
+		if (head == *stack)
+		{
+			return;
+		}
 	}
 }
